73ececa23f79c5cca4cd119579245f0a39739b3c
1107tomkim <84105893+1107tomkim@users.noreply.github.com>
2024-01-02 22:33:26 -0600

    Start of 2024 journey, back to coding

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace My_2024_Project
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello 2024");
+            Console.ReadKey();
+        }
+    }
+}
